89f9b745bb57b761208354307326ac6f3ead86ae
Christoph Korn <67979613+digital-lion@users.noreply.github.com>
2020-08-21 14:57:13 +0200

    refactored dlEventHubSend function

--- index.js
+++ index.js
@@ -9,8 +9,70 @@ module.exports = function (RED) {
         // Create the Node-RED node
         RED.nodes.createNode(this, config);
         var node = this;
-        node.on('input', function (msg) {
-            sendMessage(node, this.credentials.connectionString, this.credentials.eventHubPath, typeof(msg.payload) == 'string' ? JSON.parse(msg.payload): msg.payload);
+
+        node.on('input', async function (msg) {
+            const batchOptions = { /*e.g. batch size*/ };
+            const producerClient = new EventHubProducerClient(connectionString, eventHubPath);
+            node.log("connecting the producer client...");
+            node.status({
+                fill: 'yellow',
+                shape: 'dot',
+                text: "connecting..."
+            });
+
+            try {
+                //create new batch with options
+                var batch = await producerClient.createBatch(batchOptions);
+                node.log("create empty batch with following options:");
+                node.log(batchOptions);
+
+                //transform string to JSON if necessary
+                if (typeof (msg.payload) != "string") {
+                    node.log("Is JSON");
+                    msgJSON = msg.payload;
+                } else {
+                    node.log("Is String...converting");
+                    //Converting string to JSON Object
+                    msgJSON = JSON.parse(msg.payload);
+                }
+
+                //try to add an event to the batch
+                const isAdded = batch.tryAdd({ body: msgJSON });
+                node.log("try to add the following event to the batch:");
+                node.log(JSON.stringify(msg.payload));
+
+                if( isAdded === false ) {
+                    var warnText = "Failed to add event to the batch. Possible information loss.";
+                    node.warn(warnText);
+                    node.log(warnText);
+                }
+
+                //send batch
+                await producerClient.sendBatch(batch);
+                node.log("sent batch to event hub.");
+                node.status({
+                    fill: 'blue',
+                    shape: 'dot',
+                    text: "sent message"
+                });
+
+            } 
+            catch (err) {
+                node.log("Error when creating & sending a batch of events: ", err);
+                node.status({
+                    fill: 'red',
+                    shape: 'dot',
+                    text: "communication failed"
+                });
+            }
+            //close connection
+            await producerClient.close();
+            node.log("Disconnect producer client.");
+            node.status({
+                fill: 'grey',
+                shape: 'dot',
+                text: "connection closed"
+            });
         });
     }
 
@@ -30,73 +92,4 @@ module.exports = function (RED) {
             }
         }
     });
-    
-    /*
-    * sends the events in shape of batchs to the event hub
-    */
-    async function sendMessage(node, connectionString, eventHubPath, message) { 
-
-        const batchOptions = { /*e.g. batch size*/ };
-        const producerClient = new EventHubProducerClient(connectionString, eventHubPath);
-        node.log("connecting the producer client...");
-        node.status({
-            fill: 'yellow',
-            shape: 'dot',
-            text: "connecting..."
-        });
-
-        try {
-            //create new batch with options
-            var batch = await producerClient.createBatch(batchOptions);
-            node.log("create empty batch with following options:");
-            node.log(batchOptions);
-
-            //transform string to JSON if necessary
-            if (typeof (msg.payload) != "string") {
-                node.log("Is JSON");
-                msgJSON = msg.payload;
-            } else {
-                node.log("Is String...converting");
-                //Converting string to JSON Object
-                msgJSON = JSON.parse(msg.payload);
-            }
-
-            //try to add an event to the batch
-            const isAdded = batch.tryAdd({ body: msgJSON });
-            node.log("try to add the following event to the batch:");
-            node.log(JSON.stringify(message));
-
-            if( isAdded === false ) {
-                var warnText = "Failed to add event to the batch. Possible information loss.";
-                node.warn(warnText);
-                node.log(warnText);
-            }
-
-            //send batch
-            await producerClient.sendBatch(batch);
-            node.log("sent batch to event hub.");
-            node.status({
-                fill: 'blue',
-                shape: 'dot',
-                text: "sent message"
-            });
-
-        } 
-        catch (err) {
-            node.log("Error when creating & sending a batch of events: ", err);
-            node.status({
-                fill: 'red',
-                shape: 'dot',
-                text: "communication failed"
-            });
-        }
-        //close connection
-        await producerClient.close();
-        node.log("Disconnect producer client.");
-        node.status({
-            fill: 'grey',
-            shape: 'dot',
-            text: "connection closed"
-        });
-    };
 }
