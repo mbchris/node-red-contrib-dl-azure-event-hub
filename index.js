285b99288c0139837ad75f6f7c44489f3f5013ea
Christoph Korn <67979613+digital-lion@users.noreply.github.com>
2020-08-24 11:17:00 +0200

    added htm on edit function for debug

--- index.js
+++ index.js
@@ -119,9 +119,6 @@ module.exports = function (RED) {
             eventHubPath: {
                 type: "text"
             }
-        },
-        debug : {
-            type: "boolean"
         }
     });
 }
