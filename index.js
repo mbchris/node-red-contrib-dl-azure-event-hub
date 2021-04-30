7bfab0e26505bbff70e4feb6cf37631320560932
mbchris <schneider.chris@gmx.de>
2021-04-30 13:43:44 +0000

    changed logs to debug

--- index.js
+++ index.js
@@ -32,16 +32,15 @@ module.exports = function (RED) {
                 }
             });
             
-            node.log("connecting the producer client...");
+            node.debug("connecting the producer client...");
             node.status({
                 fill: 'yellow',
                 shape: 'dot',
                 text: "connecting..."
             });
             if(DEBUG === true) {
-                node.warn("open the producerClient connection.");
-		node.warn("params: " + node.credentials.connectionString + " - " +node.credentials.eventHubPath + " - " + node.credentials.proxyhost + " - " + node.credentials.proxyuser + " - " + node.credentials.proxypass);
-                node.warn("producerClient object:");
+                node.debug("open the producerClient connection.");
+                node.debug("producerClient object:");
                 node.send(producerClient);
             }
 
@@ -49,8 +48,8 @@ module.exports = function (RED) {
                 //create new batch with options
                 var batch = await producerClient.createBatch(batchOptions);
                 var msgJSON;
-                node.log("create empty batch with following options:");
-                node.log(batchOptions);
+                node.debug("create empty batch with following options:");
+                node.debug(batchOptions);
                 if(DEBUG === true) {
                     node.warn("create empty batch.");
                     node.warn("batchOptions:");
@@ -59,18 +58,18 @@ module.exports = function (RED) {
 
                 //transform string to JSON if necessary
                 if (typeof (msg.payload) != "string") {
-                    node.log("Is JSON");
+                    node.debug("Is JSON");
                     msgJSON = msg.payload;
                 } else {
-                    node.log("Is String...converting");
+                    node.debug("Is String...converting");
                     //Converting string to JSON Object
                     msgJSON = JSON.parse(msg.payload);
                 }
 
                 //try to add an event to the batch
                 const isAdded = batch.tryAdd({ body: msgJSON });
-                node.log("try to add the following event to the batch:");
-                node.log(JSON.stringify(msg.payload));
+                node.debug("try to add the following event to the batch:");
+                node.debug(JSON.stringify(msg.payload));
                 if(DEBUG === true) {
                     node.warn("try to add message to the batch.");
                     node.warn("message content:");
@@ -79,16 +78,15 @@ module.exports = function (RED) {
 
                 if( isAdded === false ) {
                     var warnText = "Failed to add event to the batch. Possible information loss.";
-                    node.warn(warnText);
-                    node.log(warnText);
+                    node.debug(warnText);
                     if(DEBUG === true) {
-                        node.warn("adding failed.");
+                        node.debug("adding failed.");
                     }
                 }
 
                 //send batch
                 await producerClient.sendBatch(batch);
-                node.log("sent batch to event hub.");
+                node.debug("sent batch to event hub.");
                 node.status({
                     fill: 'blue',
                     shape: 'dot',
@@ -114,7 +112,7 @@ module.exports = function (RED) {
             }
             //close connection
             await producerClient.close();
-            node.log("Disconnect producer client.");
+            node.debug("Disconnect producer client.");
             node.status({
                 fill: 'grey',
                 shape: 'dot',
